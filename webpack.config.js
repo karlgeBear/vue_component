69e9c55e610db9c81b371ba9693636a9468534a7
karlge <karlge_bear@163.com>
2021-09-03 20:31:53 +0800

    Update to Ajax

--- webpack.config.js
+++ webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
         use: {
             loader: 'babel-loader',
             options: {
-                presets: ['@babel/preset-env']
+                presets: ['@babel/preset-env'] //预设包含多个常用插件包的一个大包
             }
         }
       },
@@ -87,7 +87,7 @@ module.exports = {
       '/api': {  //匹配处理以/api开头的请求
         target: 'http://localhost:4000',  //转发的目标地址
         pathRewrite: { '^/api': '' },  //在转发请求前去除路径中/api  //htpp://localhost:4000/xxx
-        changeOrigin: true  //支持协议名的跨域
+        changeOrigin: true  //支持跨域，如果协议/主机也不相同，必须加上
       },
 
       //可匹配多个服务器
@@ -110,5 +110,5 @@ module.exports = {
     },
     extensions: ['.js','.vue'],//指定那些后缀的模块可以省略后缀
   },
-  devtool: 'eval-source-map'
+  devtool: 'source-map'
 }
