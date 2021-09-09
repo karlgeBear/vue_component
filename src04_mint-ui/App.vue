6009c6e73fef5f4f16ca29900a062e4722c754b3
karlge <karlge_bear@163.com>
2021-09-09 14:50:32 +0800

    Mint-ui

---
+++ src04_mint-ui/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <mt-button type="danger" style="width:100%" @click="test">测试Mint-ui</mt-button>
+  </div>
+</template>
+
+<script>
+  import {Toast} from 'mint-ui';
+  import { MessageBox } from 'mint-ui';
+export default {
+  methods:{
+    test(){
+      //alert('测试Mint-ui')
+      // Toast({   //text
+      //   message: 'Upload Complete',  //输入的文本
+      //   position: 'bottom',  //显示在页面的底部
+      //   duration: 5000    //在页面显示时长为5秒
+      // })
+      MessageBox({  //confirm(true)/alert(false)
+        title: 'Notice',   //显示的标题
+        message: 'Are you sure?',  //
+        showCancelButton: false
+      })
+    }
+  }
+};
+</script>
+
+<style>
+</style>
